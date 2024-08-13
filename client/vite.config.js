980d1312568f5436f7d97c997eda824e1a919a14
Ibrahim Gazi <coder.ibrahimgazi@gmail.com>
2024-08-13 06:35:35 +0600

    smaill changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL,
+        target: import.meta.env.API_URL,
         changeOrigin: true,
         secure: true,
       }
